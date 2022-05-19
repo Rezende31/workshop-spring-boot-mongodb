1715dc07533272d9661c7461ba27f186c9a41ee8
Leonardo Rezende <leo.rezende31@gmail.com>
2022-05-19 14:56:43 -0300

    Insercao de usuario com um POST

--- src/main/java/com/leonardorezende/workshopmongo/services/UserService.java
+++ src/main/java/com/leonardorezende/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leonardorezende.workshopmongo.domain.User;
+import com.leonardorezende.workshopmongo.dto.UserDTO;
 import com.leonardorezende.workshopmongo.repository.UserRepository;
 import com.leonardorezende.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+	return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());	
 	}
 }
