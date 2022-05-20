2473912cb3e87f5d999a7b38e256e6bbe00fc181
Leonardo Rezende <leo.rezende31@gmail.com>
2022-05-20 17:13:06 -0300

    Metodo para deletar usuario

--- src/main/java/com/leonardorezende/workshopmongo/resources/UserResource.java
+++ src/main/java/com/leonardorezende/workshopmongo/resources/UserResource.java
@@ -46,4 +46,10 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<UserDTO> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/leonardorezende/workshopmongo/services/UserService.java
+++ src/main/java/com/leonardorezende/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 	return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());	
 	}
