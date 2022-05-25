1acfa31626506f76fd35d8151ce47b144b46394c
Leonardo Rezende <leo.rezende31@gmail.com>
2022-05-25 13:20:36 -0300

    consulta com @Query

--- src/main/java/com/leonardorezende/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leonardorezende/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.leonardorezende.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.leonardorezende.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.leonardorezende.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
